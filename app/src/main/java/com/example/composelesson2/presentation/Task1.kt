89d36cc8e6b08b45cdc24b819da1e1ff3ce02128
brxnw <burhoniddinoff@gmail.com>
2024-03-06 11:30:13 +0500

    v5 80%

---
+++ app/src/main/java/com/example/composelesson2/presentation/Task1.kt
@@ -0,0 +1,202 @@
+package com.example.composelesson2.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composelesson2.R
+
+/*
+
+max = 343
+
+1 -> 20
+2 -> 52
+3 -> 160
+4 -> 52
+5 -> 52
+
+ */
+
+@Composable
+fun MyTask1() {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
+        // 1st Linear
+        Row(
+            modifier = Modifier
+                .weight(0.08f)
+                .fillMaxWidth()
+                .background(Color(0xFF8AA791))
+        ) {
+            // Content of the first linear layout
+        }
+
+        // 2nd Linear
+        Row(
+            modifier = Modifier
+                .weight(0.14f)
+                .fillMaxWidth()
+                .background(Color(0xFFe3ba89))
+        ) {
+
+            Spacer(modifier = Modifier.weight(4f))
+
+            Column(
+                modifier = Modifier.weight(3f)
+            ) {
+                Text(
+                    text = "MORNING",
+                    modifier = Modifier.padding(top = 10.dp),
+                    color = Color.White,
+                    fontSize = 28.sp,
+                )
+                Text(
+                    text = "- 1°",
+                    color = Color.White,
+                    fontSize = 52.sp
+                )
+            }
+        }
+
+        // 3rd Linear
+        Row(
+            modifier = Modifier
+                .fillMaxHeight(0.50f)
+                .fillMaxWidth()
+                .background(Color(0xFFd79863))
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_weather_icon),
+                contentDescription = "Weather image",
+                modifier = Modifier
+                    .size(212.dp)
+                    .padding(14.dp)
+            )
+
+//                    Spacer(modifier = Modifier.weight(4f))
+
+            Column(
+                modifier = Modifier
+                    .padding(
+                        top = 16.dp,
+                        start = 14.dp
+                    )
+                    .weight(3f)
+
+            ) {
+
+                Text(
+                    text = "DAY",
+                    modifier = Modifier.padding(top = 10.dp),
+                    color = Color.White,
+                    fontSize = 28.sp,
+                )
+
+                Text(
+                    text = "+ 3°",
+                    color = Color.White,
+                    fontSize = 52.sp
+                )
+
+                Text(
+                    text = "Mostly \nSunny",
+                    modifier = Modifier.padding(top = 20.dp),
+                    color = Color.White,
+                    fontSize = 36.sp,
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+
+                Text(
+                    text = "Wind: N5 mph\nHumidity: 45%",
+                    modifier = Modifier.padding(top = 20.dp),
+                    color = Color.White,
+                    fontSize = 22.sp,
+                    style = TextStyle(fontWeight = FontWeight.Bold)
+                )
+
+            }
+
+        }
+
+        // 4th Linear
+        Row(
+            modifier = Modifier
+                .weight(0.14f)
+                .fillMaxWidth()
+                .background(Color(0xFFb1695b))
+        ) {
+
+            Spacer(modifier = Modifier.weight(4f))
+
+            Column(
+                modifier = Modifier.weight(3f)
+            ) {
+
+                Text(
+                    text = "Evening",
+                    modifier = Modifier.padding(top = 10.dp),
+                    color = Color.White,
+                    fontSize = 28.sp
+                )
+
+                Text(
+                    text = "0°",
+                    color = Color.White,
+                    fontSize = 52.sp
+                )
+
+            }
+
+        }
+
+        // 5th Linear
+        Row(
+            modifier = Modifier
+                .weight(0.14f)
+                .fillMaxWidth()
+                .background(color = Color(0xFF644749))
+        ) {
+
+            Spacer(modifier = Modifier.weight(4f))
+
+            Column(
+                modifier = Modifier.weight(3f)
+            ) {
+
+                Text(
+                    text = "NIGHT",
+                    modifier = Modifier.padding(top = 10.dp),
+                    color = Color.White,
+                    fontSize = 28.sp
+                )
+
+                Text(
+                    text = "- 2°",
+                    color = Color.White,
+                    fontSize = 52.sp
+                )
+
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/example/composelesson2/presentation/Task11.kt
+++ app/src/main/java/com/example/composelesson2/presentation/Task11.kt
@@ -4,18 +4,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -25,10 +32,16 @@ import com.example.composelesson2.R
 @Composable
 fun Task11() {
 
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
+
 
         Column(
             modifier = Modifier
@@ -59,14 +72,197 @@ fun Task11() {
         }
 
 
+        val paddingTop = 72.dp // rasm va info line orasidagi masofa
+        val paddingBottom = 100.dp // rasm va screen pastki orasidagi masofa
+        Box(modifier = Modifier.weight(3f)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(screenHeight * 3 / 5)
+                    .padding(top = paddingTop, bottom = paddingBottom)
+            ) {
+
+                // mana shu joyga center elementlarinin qo'shing
+
+
+            }
+
+        }
 
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(3f)
                 .background(Color.White)
-        )
+        ) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.Center)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 16.dp, end = 48.dp)
+                ) {
 
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_email),
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp),
+                    )
+
+                    Text(
+                        text = "+222 202 202",
+                        modifier = Modifier.padding(start = 16.dp)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_phone),
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp),
+                    )
+                    Text(text = "+222 202 202", modifier = Modifier.padding(start = 16.dp))
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_group_add),
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp),
+                    )
+                    Text(text = "+222 202 202", modifier = Modifier.padding(start = 16.dp))
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_message),
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp),
+                    )
+                    Text(
+                        text = "+222 202 202",
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                    )
+                }
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(top = 16.dp),
+                    colors = ButtonDefaults
+                        .buttonColors(containerColor = Color(0xFF09C4DD))
+                ) {
+                    Text(text = "FOLLOW ME", color = Color.White)
+
+                }
+            }
+// asd
+        }
+
+
+    }
+
+    Box(
+        modifier = Modifier
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = screenHeight * 2 / 5 - 50.dp,
+                bottom = screenHeight * 3 / 5 - 50.dp
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .shadow(
+                    elevation = 8.dp,
+                    spotColor = Color.Black,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .background(Color.White, RoundedCornerShape(12.dp))
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = "Photos",
+                    fontSize = 12.sp,
+                    color = Color(0xFF858080)
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .align(Alignment.CenterHorizontally),
+                    text = "376",
+                    fontWeight = FontWeight.W500,
+                    fontSize = 28.sp,
+                    color = Color(0xFF32B3ED)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = "Followers",
+                    fontSize = 12.sp,
+                    color = Color(0xFF858080)
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .align(Alignment.CenterHorizontally),
+                    text = "1769",
+                    fontWeight = FontWeight.W500,
+                    fontSize = 28.sp,
+                    color = Color(0xFF858080)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = "Following",
+                    fontSize = 12.sp,
+                    color = Color(0xFF858080)
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .align(Alignment.CenterHorizontally),
+                    text = "127",
+                    fontWeight = FontWeight.W500,
+                    fontSize = 28.sp,
+                    color = Color(0xFF858080)
+                )
+            }
+
+
+        }
     }
 
 }
